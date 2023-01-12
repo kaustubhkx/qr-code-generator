ec01d179bc488126bf30cc7b3d332549ff6f8809
kaustubhkx <kaustubhkrishna2003@gmail.com>
2023-01-12 19:14:20 +0530

    Made Changes in gui-main.py

--- gui-main.py
+++ gui-main.py
@@ -1,20 +1,26 @@
 from tkinter import *
+from tkinter.filedialog import askopenfile
 import qrcode
 root = Tk()
 root.geometry("1000x500")
 root.title("QR Code Generator")
 QR_Code_Generator = Label(root, text="QR Code Generator", font="Ariel").pack(side=TOP)
 Enter_URL = Label(text="Enter URL").pack()
+
 def save():
     a=str(get_url.get())
     img = qrcode.make(a)
     img.save(save_url.get() +".jpg")
-    print("QR Code Generated")
+    generated_successfully = Label(text="QR Code Generated Successfully")
 get_url = Entry(root)
 get_url.pack()
 enter_url = Label(text="Enter Name of the file").pack()
 save_url = Entry(root)
 save_url.pack()
+def delete():
+    get_url.delete(0, END)
+    save_url.delete(0, END)
 button = Button(text="Generate QR Code", command=save).pack()
-
+button_clear = Button(text="Clear", command=delete)
+button_clear.pack()
 root.mainloop()
