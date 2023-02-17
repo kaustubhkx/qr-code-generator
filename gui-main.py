a83c524afeca4f6a198f3095a5622d3ed084f651
kaustubhkx <kaustubhrx@gmail.com>
2023-02-17 19:08:11 +0530

    Added error message for blank input fields in GUI
    Previously, if the user left a required field blank in the GUI, no feedback was given and the application would proceed with incomplete data. To improve the user experience, this commit adds a new feature in gui-main.py that displays an error dialog box when a required field is left blank. The error message provides specific instructions on how to correct the error and prompts the user to enter valid input. This change enhances the usability of the application and reduces the likelihood of errors caused by incomplete data.

--- gui-main.py
+++ gui-main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
 import qrcode
+from tkinter import messagebox 
 
 root = Tk()
 root.geometry("1000x500")
@@ -11,19 +12,29 @@ Enter_URL = Label(text="Enter URL").pack()
 get_url = Entry(root, width=50)
 get_url.pack()
 
+ 
 def save():
-    global generated_successfully
-    file = filedialog.asksaveasfilename(filetypes=[('JPEG files','*.jpeg')],defaultextension='.png')
-    a=str(get_url.get())
-    img = qrcode.make(a)
-    img.save(file)
-    generated_successfully = Label(text="QR Code Generated Successfully")
-    generated_successfully.pack()
+    if not get_url.get():
+        messagebox.showerror("Empty field", "This field cannot be left blank")
+    else:
+        try:
+            global generated_successfully   
+            file = filedialog.asksaveasfilename(filetypes=[('JPEG files','*.jpeg')],defaultextension='.png')   
+            a=str(get_url.get())   
+            img = qrcode.make(a)   
+            img.save(file) 
+            generated_successfully = Label(text="QR Code Generated Successfully")    
+            generated_successfully.pack()
+        except:
+            messagebox.showerror("Invalid URL","Enter a valid URL")
 
 
 def delete():
-    get_url.delete(0, END)
-    generated_successfully.destroy()
+    try:
+        get_url.delete(0, END)
+        generated_successfully.destroy()
+    except:
+        pass
 
 button = Button(text="Generate QR Code", command=save).pack()
 button_clear = Button(text="Clear", command=delete)
